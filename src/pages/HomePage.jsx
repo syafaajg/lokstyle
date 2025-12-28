594a7b35026a325a51dabe2048b9676b1affc9e6
syafaajg <syafaajggggg@gmail.com>
2025-12-28 22:03:28 +0700

    Add files via upload

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import ProductList from '../components/ProductList';
+
+const HomePage = () => {
+    return (
+        <>
+            <Hero />
+            <ProductList />
+        </>
+    );
+};
+
+export default HomePage;
